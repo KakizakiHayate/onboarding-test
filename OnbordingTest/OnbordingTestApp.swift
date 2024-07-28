aa8b6645586d507ea298ba26b54e78b9d14b1abf
hayate <hayate.k.0704@gmail.com>
2024-07-28 23:23:47 +0900

    initial files

---
+++ OnbordingTest/OnbordingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnbordingTestApp.swift
+//  OnbordingTest
+//
+//  Created by 柿崎 on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct OnbordingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
