fd740d73cd74f4c20fa07878650d63d1d33d77f3
hayate <hayate.k.0704@gmail.com>
2024-07-28 23:51:41 +0900

    tabviewのインジケータの色を変更することに成功

--- OnbordingTest/ContentView.swift
+++ OnbordingTest/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let onboarding: [OnboardingData] = onboardingData
+
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = .cyan
+        UIPageControl.appearance().pageIndicatorTintColor = .brown
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let pageCount = onboarding.count
+        ZStack {
+            Color.red
+                .ignoresSafeArea()
+            VStack {
+                TabView {
+                    ForEach(onboarding[0 ..< pageCount]) { _ in
+                        Text("fdsklfjdskljf")
+                    }
+                }
+                .tabViewStyle(.page)
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .interactive))
+            }
         }
-        .padding()
     }
 }
+
+struct OnboardingData: Identifiable {
+    var id = UUID()
+    let pageNumber: Int
+    let imageName: String
+}
+
+let onboardingData: [OnboardingData] = [
+    .init(pageNumber: 1, imageName: "path"),
+    .init(pageNumber: 1, imageName: "path"),
+    .init(pageNumber: 1, imageName: "path")
+]
 
 #Preview {
     ContentView()
